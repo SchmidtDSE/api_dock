e6f9f8df14907067482f3be5864e504df8227829
brookie <bguzder-williams@berkeley.edu>
2025-09-27 17:44:28 +0000

    Export RouteMapper class in module __init__.py

--- api_box/__init__.py
+++ api_box/__init__.py
@@ -10,5 +10,6 @@ License: CC-BY-4.0
 
 from .main import app, create_app
 from .config import load_main_config, find_remote_config, get_remote_names
+from .route_mapper import RouteMapper
 
-__all__ = ["app", "create_app", "load_main_config", "find_remote_config", "get_remote_names"]
+__all__ = ["app", "create_app", "load_main_config", "find_remote_config", "get_remote_names", "RouteMapper"]
